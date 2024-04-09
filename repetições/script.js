b26ca25b4cd3aced8336e10221b9e184bdca1d29
Tainá Cunha <166453655+Luuuzzzzzz@users.noreply.github.com>
2024-04-08 22:06:05 -0300

    Add files via upload

---
+++ repetições/script.js
@@ -0,0 +1,35 @@
+
+// let resultado = document.getElementById('divResultado');
+// console.log(resultado);
+
+// let nome = prompt("Qual é o seu nome: ")
+// resultado.innerHTML = "Olá, eu sou uma div... legal né"
+
+// let cont = 0
+// while(cont < 10){
+//     // console.log(cont);
+//     resultado.innerHTML += cont + " patinho foi passear... <br>"
+//     cont++
+// }
+
+// while(cont > 0){
+//     resultado.innerHTML += cont + " patinho voltou de lá...<br>"
+//     cont--
+// }
+// Faça um programa que calcule o quadrado de cada número de 1 a 10.
+
+
+
+// let quadrado = document.getElementById('quadrado')
+// let contador = 1
+// while(contador < 11){
+//     quadrado.innerHTML += "<br>" + contador ** 2 + " é o quadrado do contador " + contador + "<br>"
+//     contador++
+// }
+
+let decrescente = document.getElementById('decrescente')
+let contador = 10
+while(contador > 0){
+    decrescente.innerHTML += "<br>" + contador
+    contador--
+}
