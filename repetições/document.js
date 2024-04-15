f10ea2c5116d0fdf146a623489b299b7157509a8
TAINÁ MACHADO DA CUNHA <taina_cunha@estudante.sesisenai.org.br>
2024-04-15 19:41:47 -0300

    teste de commit

---
+++ repetições/document.js
@@ -0,0 +1,116 @@
+// let resultado = document.getElementById('divResultado');
+// console.log(resultado);
+
+// let nome = prompt("Qual é o seu nome: ")
+// resultado.innerHTML = "Olá, eu sou uma div... legal né"
+
+// let cont = 0
+// while(cont < 10){
+//     // console.log(cont);
+//     resultado.innerHTML += cont + " patinho foi passear... <br>"
+//     cont++
+// }
+
+// while(cont > 0){
+//     resultado.innerHTML += cont + " patinho voltou de lá...<br>"
+//     cont--
+// }
+// Faça um programa que calcule o quadrado de cada número de 1 a 10.
+
+
+
+// let quadrado = document.getElementById('quadrado')
+// let contador = 1
+// while(contador < 11){
+//     quadrado.innerHTML += "<br>" + contador ** 2 + " é o quadrado do contador " + contador + "<br>"
+//     contador++
+// }
+
+
+
+
+// let decrescente = document.getElementById('decrescente')
+// let contador = 10
+// while(contador > 0){
+//     decrescente.innerHTML += "<br>" + contador
+//     contador--
+// }
+
+
+
+
+// Imprima os números de 10 a 1 em ordem decrescente.
+
+// let decrescente = document.getElementById("decrescente")
+// let contador = 10
+// while(contador >= 1){
+//     decrescente.innerHTML += '<br>' + contador
+//     contador -= 1
+// }
+
+//Calcule a soma dos números de 1 a 100.
+
+// let soma = 0;
+// let contador = 1;
+// while (contador <= 100) {
+//     soma += contador;
+//     contador++;
+// }
+// document.getElementById("cem").innerHTML = `A soma de 1 a 100 é: ${soma}`;
+
+//Imprima a tabuada do 5.
+
+// let tabuada = document.getElementById("tabuada")
+// let contador = 0
+// let multip = 5
+// while(contador <= 10){
+//     tabuada.innerHTML += "<br>" + contador * multip
+//     contador++
+// }
+
+// Altere o programa anterior para fornecer a tabuada do número que o usuário pedir.
+
+// let tabuada = document.getElementById("tabuadaOnline")
+// let contador = 0
+// let multip = parseInt(prompt("Escolha um número e descubra a tabuada do mesmo:"))
+// while(contador <= 10){
+//     tabuadaOnline.innerHTML += "<br>" + contador * multip
+//     contador++
+// }
+
+
+// Imprima os números ímpares de 1 a 20.
+
+// let impares = document.getElementById("impares")
+// let numero = 1
+// while(numero <= 20){
+//     if(numero % 2 !== 0){
+//         impares.innerHTML += "<br>" + numero
+//             }
+//             numero++
+// }
+
+// Imprima os números de 1 a 10 em ordem crescente, depois imprima-os em ordem decrescente.
+
+// let output = document.getElementById("output");
+// let contadorCresc = 1;
+// while(contadorCresc <= 10){
+//     output.innerHTML += "<br>" + contadorCresc++
+//         }
+
+// let contadorDecres = 10;
+// while(contadorDecres >= 1){
+//     output.innerHTML += "<br>" + contadorDecres--
+//    }
+
+//Calcule o fatorial de um número dado.
+
+// let fatorial = document.getElementById('fatorial');
+// let numero = parseInt(prompt('Escolha um número para ver o fatorial dele: '))
+// let resultado = 1
+// let contador = 1
+// while(contador <= numero){
+//     fatorial.innerHTML +=  "<br>" + (resultado *= contador) 
+//     contador++
+// }
+
